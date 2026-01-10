35f0af49f6c690c67547a15bf3799f71ed73cfc4
Vihanga <vdeshan044@gmail.com>
2026-01-10 09:45:09 +0530

    Initial Commit

---
+++ square_game/square_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  square_gameApp.swift
+//  square_game
+//
+//  Created by COBSCCOMP24.2P-008 on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct square_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
